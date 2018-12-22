841c9d52b7407ce4efa0648598b3b58ece90c8d6
Agrawal <Navneet.Agrawal@FISGLOBAL.COM>
2018-12-22 12:31:31 +0530

    poker scrum

---
+++ precache-manifest.cd73325091105885d29b1b48747ff14e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84796716dc7bdf95afc6",
+    "url": "/static/js/main.84796716.chunk.js"
+  },
+  {
+    "revision": "f648ca66289e9c3f15b7",
+    "url": "/static/js/1.f648ca66.chunk.js"
+  },
+  {
+    "revision": "84796716dc7bdf95afc6",
+    "url": "/static/css/main.8735403a.chunk.css"
+  },
+  {
+    "revision": "f648ca66289e9c3f15b7",
+    "url": "/static/css/1.ddf0f32a.chunk.css"
+  },
+  {
+    "revision": "f37b56d4b27da22583ccd43539d51c7a",
+    "url": "/index.html"
+  }
+];
